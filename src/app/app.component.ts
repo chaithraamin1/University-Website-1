212063b579daa256c4ddcf607f52314a37126fe6
Chaithra Amin <chaithra.amin98@gmail.com>
2022-09-08 12:27:47 +0530

    added Email subscription code which link with excel file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mail_subscription_with_google';
+  ngOnInit(){
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxZCmvQLD3ScBlR83-Y9eyD_dAmOKPkBdNyno5LvDgMwcHZl_KOzRddtTo5QXfu35ck/exec';
+    const newLocal:any = 'submit-to-google-sheet';
+    const form = document.forms[newLocal];
+    const msg:any =document.getElementById("msg");
+    form.addEventListener('submit', function (e:any) {
+        debugger;
+        e.preventDefault();
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then(response => {
+            msg.innerHTML='Thank You For subscribing!'
+            setTimeout(function(){
+              msg.innerHTML="";
+            },3000)
+           form.reset()
+          })
+          .catch(error => console.error('Error!', error.message));
+      })
+  }
 }
